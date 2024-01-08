d95e29d45c4da58b6c061ecf63b02f9d819de526
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-01-08 12:46:08 +0530

    deployments mods

--- script/DeployUSDC.s.sol
+++ script/DeployUSDC.s.sol
@@ -13,13 +13,15 @@ import {FiatTokenV2_1} from "../src/FiatTokenV2_1/centre-tokens/contracts/v2/Fia
 
 contract DeployUSDCScript is Script {
     FiatTokenProxy proxyContract;
-    FiatTokenV2_1 fiatTokenV2_2;
+    FiatTokenV2_1 fiatTokenV2_1;
 
-    uint256 public deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    address public proxyOwner = vm.envAddress("OWNER");
-    address public minterConfigurator = vm.envAddress("MINTING_ADMIN");
-    address public erc20Vault = vm.envAddress("ERC20_VAULT");
+    address public proxyOwner = 'A PROXY OWNER - Different than minter configurator';
+    uint256 public proxyOwnerPrivKey = 'PROXY_OWNER_PRIV_KEY';
+    
+    address public minterConfigurator = 'AN_EOA_FOR_NOW_FOR_TESTING';
+    uint256 public minterRolePrivKey = 'MINTER_ROLE_PRIV_KEY';
 
+    address public erc20Vault = 'Erc20Vault on Katla';
 
     address THROWAWAY_ADDRESS = 0x0000000000000000000000000000000000000001;
 
@@ -35,18 +37,23 @@ contract DeployUSDCScript is Script {
         require(minterConfigurator != address(0), "minter admin is zero");
         require(erc20Vault != address(0), "erc20Vault is zero");
 
-        vm.startBroadcast(deployerPrivateKey);
-        fiatTokenV2_2 = new FiatTokenV2_1();
-        proxyContract = new FiatTokenProxy(address(fiatTokenV2_2));
+        vm.startBroadcast(proxyOwnerPrivKey);
+        fiatTokenV2_1 = new FiatTokenV2_1();
+        console2.log("Address of fiatTokenV2_1:", address(fiatTokenV2_1));
+        proxyContract = new FiatTokenProxy(address(fiatTokenV2_1));
+        console2.log("Address of proxy:", address(proxyContract));
 
         //// These values are dummy values because we only rely on the implementation
         //// deployment for delegatecall logic, not for actual state storage.
-        fiatTokenV2_2.initialize("", "", "", 0, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS);
-        fiatTokenV2_2.initializeV2("");
-        fiatTokenV2_2.initializeV2_1(THROWAWAY_ADDRESS);
+        fiatTokenV2_1.initialize("", "", "", 0, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS, THROWAWAY_ADDRESS);
+        fiatTokenV2_1.initializeV2("");
+        fiatTokenV2_1.initializeV2_1(THROWAWAY_ADDRESS);
 
+        vm.stopBroadcast();
+
+        vm.startBroadcast(minterRolePrivKey);
         //// Do the V1 initialization
-        // console.log("Initializing V1..
+        console2.log("Initializing V1..");
         FiatTokenV2_1(address(proxyContract)).initialize(
             NAME,
             SYMBOL,
@@ -69,13 +76,12 @@ contract DeployUSDCScript is Script {
         FiatTokenV2_1(address(proxyContract)).initializeV2_1(
             THROWAWAY_ADDRESS
         );
-        
-        vm.stopBroadcast();
 
-        vm.prank(minterConfigurator, minterConfigurator);
         FiatTokenV2_1(address(proxyContract)).configureMinter(
             erc20Vault,
             type(uint256).max
         );
+
+        vm.stopBroadcast();
     }
 }
