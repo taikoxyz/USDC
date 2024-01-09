429896d0991391fbb9dfda606984f7e5316f9723
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-01-09 11:35:00 +0530

    Update to circle V2_2

--- src/FiatTokenV2_1/centre-tokens/contracts/interface/IERC1271.sol
+++ src/FiatTokenV2_1/centre-tokens/contracts/interface/IERC1271.sol
@@ -18,18 +18,19 @@
 
 pragma solidity 0.6.12;
 
-import { AbstractFiatTokenV1 } from "../v1/AbstractFiatTokenV1.sol";
-
-abstract contract AbstractFiatTokenV2 is AbstractFiatTokenV1 {
-    function _increaseAllowance(
-        address owner,
-        address spender,
-        uint256 increment
-    ) internal virtual;
-
-    function _decreaseAllowance(
-        address owner,
-        address spender,
-        uint256 decrement
-    ) internal virtual;
+/**
+ * @dev Interface of the ERC1271 standard signature validation method for
+ * contracts as defined in https://eips.ethereum.org/EIPS/eip-1271[ERC-1271].
+ */
+interface IERC1271 {
+    /**
+     * @dev Should return whether the signature provided is valid for the provided data
+     * @param hash          Hash of the data to be signed
+     * @param signature     Signature byte array associated with the provided data hash
+     * @return magicValue   bytes4 magic value 0x1626ba7e when function passes
+     */
+    function isValidSignature(bytes32 hash, bytes memory signature)
+        external
+        view
+        returns (bytes4 magicValue);
 }
